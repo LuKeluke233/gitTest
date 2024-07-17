76dcb806a1524a7ac43e1838d34225798193a0e5
chenbo <chenbo@wiseda.com.cn>
2024-07-17 13:02:53 +0800

    333

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 
-<<<<<<< HEAD
-console.log('这是测试提交')
-=======
 console.log('这是测试提交')
 console.log('这是测试提交')
 console.log('这是测试提交')
 console.log('这是测试提交')
 console.log('这是测试提交')
->>>>>>> 4de5578 (222)
